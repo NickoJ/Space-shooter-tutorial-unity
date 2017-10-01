81057d35e885f4423194e453b3ae2d2e06d21aa0
Nickolay Klyukay <klyukay@gmail.com>
2017-10-01 19:20:40 +0300

    06. Creating shots

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+
+	public float speed = 1f;
+
+	Rigidbody body;
+
+	void Awake()
+	{
+		body = GetComponent<Rigidbody> ();
+	}
+
+	void Start()
+	{
+		body.velocity = transform.forward * speed * Time.deltaTime;
+	}
+
+}	
