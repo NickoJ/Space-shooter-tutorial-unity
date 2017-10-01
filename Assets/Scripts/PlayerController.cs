0b92cda7acf4da4377013b9374f89e1d8dc63b99
Nickolay Klyukay <klyukay@gmail.com>
2017-10-01 21:43:00 +0300

    07. Shooting shots

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,11 +10,25 @@ public class PlayerController : MonoBehaviour
 	public float tilt;
 	public Boundary boundary;
 
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
 	Rigidbody body;
+	float nextFire;
 
 	void Awake()
 	{
 		body = GetComponent<Rigidbody> ();
+	}
+
+	void Update()
+	{
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) 
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+		}
 	}
 
 	void FixedUpdate()
