61757651dae3214221560dc5afca3845bacc68fe
Nickolay Klyukay <klyukay@gmail.com>
2017-10-02 21:16:34 +0300

    03. Explosions

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -3,9 +3,17 @@
 public class DestroyByContact : MonoBehaviour
 {
 
+	public GameObject explosion;
+	public GameObject playerExplosion;
+
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.CompareTag ("Boundary")) return;
+		Instantiate (explosion, transform.position, transform.rotation);
+		if (other.CompareTag ("Player")) 
+		{
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+		}
 		Destroy (other.gameObject);
 		Destroy (gameObject);
 	}
