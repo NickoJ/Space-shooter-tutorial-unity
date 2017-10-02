6818c0ebb329793ff2922941bcc96a078ab12b69
Nickolay Klyukay <klyukay@gmail.com>
2017-10-02 20:43:03 +0300

    02. Creating hazards

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class RandomRotator : MonoBehaviour 
+{
+
+	public float tumble;
+
+	void Start()
+	{
+		var body = GetComponent<Rigidbody> ();
+		body.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+
+}
